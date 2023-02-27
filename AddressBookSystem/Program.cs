663fb18623ca005cf1d4966e12f1f99df78a4b4c
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 21:07:52 +0530

    [Prabhu] Add: Contact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System");
+            Contact contact = new Contact();
         }
     }
 }

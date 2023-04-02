e099340aa534a77f735ac44bc72231d4e59d46d1
rishiraj007 <rishiraj@mounttalent.com>
2023-04-02 23:48:19 +0530

    [Prabhu]Add: Read and Write Person using CSV File

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -17,7 +17,8 @@ public class Program
             Console.WriteLine("\nEnter your choice \n1.Add Contacts \n2.View Contacts \n3.Edit Contact \n4.Delete Contact " +
                 "\n5.Add Multiple Person \n6.Add Multiple AddressBook using Dictionary \n7.Display List of Dictionary \n8.Check for Duplicate Contacts " +
                 "\n9.Search Person by City or State \n10.View Person by City or State \n11.Count contact \n12.Sort Contact by Name " +
-                "\n13.Sort Contact By City _State_ZipCode \n14.File Exit or Not \n15.Read Person File \n16.Write Person File \n17.Exit");
+                "\n13.Sort Contact By City _State_ZipCode \n14.File Exit or Not \n15.Read Person File \n16.Write Person File \n17.Person Contact as CSV file " +
+                "\n18.Exit");
             Console.WriteLine("Enter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -71,6 +72,9 @@ public class Program
                     break;
                 case 16:
                     fileIO.WriterPersonsContactsFile(personsContact);
+                    break;
+                case 17:
+                    PersonsContactsAsCSVFile.ReadCSVWriteCSV();
                     break;
                 default:
                     flag = false;
